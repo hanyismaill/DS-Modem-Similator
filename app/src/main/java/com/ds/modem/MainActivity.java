b9c1d2d941052f7e8ab728cab926472a96b17705
hanyismaill <ohany.ismailo@gmail.com>
2026-03-26 00:44:20 +0200

    Refactor MainActivity to extend Activity
    Changed MainActivity to extend Activity instead of AppCompatActivity and modified onCreate method to set initial text and background color.

--- app/src/main/java/com/ds/modem/MainActivity.java
+++ app/src/main/java/com/ds/modem/MainActivity.java
@@ -1,13 +1,12 @@
 package com.ds.modem;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     TextView screen;
-    int stage = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +18,8 @@ public class MainActivity extends AppCompatActivity {
         screen.setTextSize(16);
         screen.setPadding(20, 40, 20, 20);
 
+        screen.setText("iDIRECT Modem Virtualization\n\nReady...");
+
         setContentView(screen);
-
-        startCLI();
-    }
-
-    private void startCLI() {
-        if (stage == 0) {
-            screen.setText("Username: admin\nPassword: ");
-            stage = 1;
-        } else if (stage == 1) {
-            screen.append("\n\n[RMT:17231] admin@telnet:127.0.0.1;4494\n");
-        }
     }
 }
